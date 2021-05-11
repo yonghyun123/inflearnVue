e4c21208b36135b0c3308a06a09b843f60b48eca
yonghyun123 <model9960@naver.com>
2021-05-11 23:17:45 +0900

    adding todo input style

---
+++ vue-todo/src/components/TodoHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <h1>Todo It</h1>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+h1 {
+  color: #2f3e52;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
+}
+</style>
+
